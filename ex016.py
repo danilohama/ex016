c30cc24a5072d07518f9ee6738318fc4c831c27c
Danilo ohama <91641703+danilohama@users.noreply.github.com>
2023-09-05 20:16:49 -0300

    Add files via upload

---
+++ ex016.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia um numero Real qualquer pelo teclado e mostre sua porção inteira.
+""" from math import trunc
+num = float(input('Digite um valor: '))
+
+print('O valor {} e a sua porção inteira é {}'.format(num, trunc(num)))
+"""
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
